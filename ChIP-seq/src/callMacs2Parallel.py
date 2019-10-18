7ce95ec297a146eb22d9e8d0db84aed953bee66e
Keren Chow <34370908+KR-Chow@users.noreply.github.com>
2019-10-17 23:17:55 -0700

    Update callMacs2Parallel.py

--- ChIP-seq/src/callMacs2Parallel.py
+++ ChIP-seq/src/callMacs2Parallel.py
@@ -13,6 +13,9 @@ import subprocess
 parser = argparse.ArgumentParser()
 parser.add_argument('-cpu', action='store', type=int,
                     default=10, help='cores for multiprocessing')
+parser.add_argument('-dtype', action='store', type=str,
+                    default='narrowPeak', help='The --input-file-type \
+                    (narrowPeak|broadPeak|bed|gff)')
 parser.add_argument('-ip', nargs='+', type=str,
                     help='The ip files')
 parser.add_argument('-input', nargs='+', type=str,
@@ -33,24 +36,35 @@ parser.add_argument('-pval', action='store', type=float,
                     default=1e-2, help='The p-value cutoff')
 parser.add_argument('-other', action='store', type=str,
                     help='The other commands, "," separated (eg.--broad,--broad)')
+parser.add_argument('-rank', action='store', type=str,
+                    default='signal.value', help='Which column to use to rank peaks \
+                    (idr, signal.value|p.value|q.value)')
 
 args = parser.parse_args()
 if len(sys.argv[1:]) == 0:
     parser.print_help()
     parser.exit()
 
-def runMacs2(ip, control, name, other, output, extsize, gsize, shift, pval, dFormat):
-    try:
-        shutil.rmtree(output)
-    except FileNotFoundError as e:
-        pass
-    os.makedirs(output, exist_ok=True)
+def runMacs2(ip, control, name, other, output, extsize, gsize, shift, pval, dFormat, dtype, rank):
+    # calling peaks
     macs2Command = 'macs2 callpeak -t {ip} \
         -c {control} -f {dFormat} -g {gsize} \
         -p {pval} --nomodel --shift {shift} \
         --extsize {extsize} -n {name} --outdir {output} \
         {other} > {output}/../{name}.macs2.log 2>&1'.format(**vars())
     subprocess.run(macs2Command, shell=True)
+    # sort peaks by signal.value | -log10(p-value) | -log10(p-value)
+    peakFile = os.path.join(output, name + '_peaks.{0}'.format(dtype))
+    broadFlag = False
+    if 'broadPeak' in dtype:
+        broadFlag = True
+    if rank == 'signal.value':
+        sortCommand = 'sort -k7,7n {0} -o {0}'.format(peakFile)
+    elif rank == 'p.value':
+        sortCommand = 'sort -k8,8nr {0} -o {0}'.format(peakFile)
+    else:
+        sortCommand = 'sort -k9,9nr {0} -o {0}'.format(peakFile)
+    subprocess.run(sortCommand, shell=True)
     return 1
 
 # public variables
@@ -64,6 +78,8 @@ gsize = args.gsize
 shift = args.shift
 pval = args.pval
 dFormat = args.format
+dtype = args.dtype
+rank = args.rank
 
 # judge elements in same length
 if len(ipList) == len(inputList) and len(ipList) == len(nameList):
@@ -88,9 +104,15 @@ for i in range(len(ipList)):
     name = nameList[i]
     other = otherList[i]
     output = outputList[i]
+    # try to make dirs
+    try:
+        shutil.rmtree(output)
+    except FileNotFoundError as e:
+        pass
+    os.makedirs(output, exist_ok=True)
     #runMacs2(ip, control, name, other, output, extsize, gsize, shift, pval, dFormat)
     result = pool.apply_async(runMacs2, args=(ip, control, name, other, 
-        output, extsize, gsize, shift, pval, dFormat))
+        output, extsize, gsize, shift, pval, dFormat, dtype, rank))
 pool.close()
 pool.join()
 
