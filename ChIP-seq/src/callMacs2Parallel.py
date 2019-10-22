f394b260ea90680493d48e90b16b548af1b9ffa1
Keren Chow <34370908+KR-Chow@users.noreply.github.com>
2019-10-22 10:02:29 -0700

    Update callMacs2Parallel.py

--- ChIP-seq/src/callMacs2Parallel.py
+++ ChIP-seq/src/callMacs2Parallel.py
@@ -13,6 +13,8 @@ import subprocess
 parser = argparse.ArgumentParser()
 parser.add_argument('-cpu', action='store', type=int,
                     default=10, help='cores for multiprocessing')
+parser.add_argument('-chrsize', action='store', type=str,
+                    help='chromosome size file (eg. hg38.chrom.sizes)')
 parser.add_argument('-dtype', action='store', type=str,
                     default='narrowPeak', help='The --input-file-type \
                     (narrowPeak|broadPeak|bed|gff)')
@@ -45,14 +47,16 @@ if len(sys.argv[1:]) == 0:
     parser.print_help()
     parser.exit()
 
-def runMacs2(ip, control, name, other, output, extsize, gsize, shift, pval, dFormat, dtype, rank):
+def runMacs2(ip, control, name, other, output, extsize, gsize, shift, pval, dFormat, dtype, rank, chrsize):
     # calling peaks
+    logFile = '{output}/../{name}.macs2AndTrack.log 2>&1'.format(**vars())
     macs2Command = 'macs2 callpeak -t {ip} \
         -c {control} -f {dFormat} -g {gsize} \
         -p {pval} --nomodel --shift {shift} \
         --extsize {extsize} -n {name} --outdir {output} \
-        {other} > {output}/../{name}.macs2.log 2>&1'.format(**vars())
+        {other} > {logFile} 2>&1'.format(**vars())
     subprocess.run(macs2Command, shell=True)
+
     # sort peaks by signal.value | -log10(p-value) | -log10(p-value)
     peakFile = os.path.join(output, name + '_peaks.{0}'.format(dtype))
     broadFlag = False
@@ -65,6 +69,58 @@ def runMacs2(ip, control, name, other, output, extsize, gsize, shift, pval, dFor
     else:
         sortCommand = 'sort -k9,9nr {0} -o {0}'.format(peakFile)
     subprocess.run(sortCommand, shell=True)
+
+    # generate signal track
+    if broadFlag is False:
+        prefix = '{output}/{name}'.format(**vars())
+        signalCommand = 'macs2 bdgcmp -t {prefix}_treat_pileup.bdg \
+            -c {prefix}_control_lambda.bdg \
+            --outdir {output} -o {name}_FE.bdg -m FE >> {logFile} 2>&1'.format(**vars())
+        subprocess.run(signalCommand, shell=True)
+
+        # Remove coordinates outside chromosome sizes (stupid MACS2 bug)
+        removeCommand = "slopBed -i {prefix}_FE.bdg -g {chrsize} \
+            -b 0 | awk '{{if ($3 != -1) print $0}}' | bedClip stdin {chrsize} \
+            {prefix}.fc.signal.bedgraph >> {logFile} 2>&1".format(**vars())
+        subprocess.run(removeCommand, shell=True)
+        os.remove('{prefix}_FE.bdg'.format(**vars()))
+
+        # Convert bedgraph to bigwig
+        sortCommand = 'sort -k1,1 -k2,2n {prefix}.fc.signal.bedgraph \
+            -o {prefix}.fc.signal.bedgraph'.format(**vars())
+        subprocess.run(sortCommand, shell=True)
+        mergeCommand = 'bedtools merge -i {prefix}.fc.signal.bedgraph -c 4 -d -1 -o max \
+            > {prefix}.fc.signal.sorted.bedgraph'.format(**vars())
+        subprocess.run(mergeCommand, shell=True)
+        sortCommand = 'sort -k1,1 -k2,2n {prefix}.fc.signal.sorted.bedgraph \
+            -o {prefix}.fc.signal.sorted.bedgraph'.format(**vars())
+        subprocess.run(sortCommand, shell=True)
+        convertCommand = "bedGraphToBigWig {prefix}.fc.signal.sorted.bedgraph \
+            {chrsize} {prefix}.fc.signal.bw >> {logFile} 2>&1".format(**vars())
+        subprocess.run(convertCommand, shell=True)
+        os.remove('{0}.fc.signal.bedgraph'.format(prefix))
+        os.remove('{0}.fc.signal.sorted.bedgraph'.format(prefix))
+        os.remove('{0}_treat_pileup.bdg'.format(prefix))
+        os.remove('{0}_control_lambda.bdg'.format(prefix))
+
+        # generate count signal track
+        ## postive strand
+        countCommand = "zcat -f {ip} | sort -k1,1 -k2,2n | \
+            bedtools genomecov -5 -bg -strand + -g {chrsize} \
+            -i stdin > {output}/TMP.POS.BED".format(**vars())
+        subprocess.run(countCommand, shell=True)
+        countCommand = "bedGraphToBigWig {output}/TMP.POS.BED \
+            {chrsize} {output}/{name}.positive.bw >> {logFile} 2>&1".format(**vars())
+        subprocess.run(countCommand, shell=True)
+        ## negative strand
+        countCommand = "zcat -f {ip} | sort -k1,1 -k2,2n | \
+            bedtools genomecov -5 -bg -strand - -g {chrsize} \
+            -i stdin > {output}/TMP.POS.BED".format(**vars())
+        subprocess.run(countCommand, shell=True)
+        countCommand = "bedGraphToBigWig {output}/TMP.POS.BED {chrsize} \
+            {output}/{name}.negative.bw >> {logFile} 2>&1".format(**vars())
+        subprocess.run(countCommand, shell=True)
+        os.remove('{output}/TMP.POS.BED'.format(**vars()))
     return 1
 
 # public variables
@@ -72,6 +128,7 @@ ipList = args.ip
 inputList = args.input
 nameList = args.name
 otherList = args.other.split(',')
+chrsize = args.chrsize
 outputList = args.output
 extsize = args.extsize
 gsize = args.gsize
@@ -110,9 +167,10 @@ for i in range(len(ipList)):
     except FileNotFoundError as e:
         pass
     os.makedirs(output, exist_ok=True)
-    #runMacs2(ip, control, name, other, output, extsize, gsize, shift, pval, dFormat)
+    #runMacs2(ip, control, name, other, 
+    #    output, extsize, gsize, shift, pval, dFormat, dtype, rank, chrsize)
     result = pool.apply_async(runMacs2, args=(ip, control, name, other, 
-        output, extsize, gsize, shift, pval, dFormat, dtype, rank))
+        output, extsize, gsize, shift, pval, dFormat, dtype, rank, chrsize))
 pool.close()
 pool.join()
 
