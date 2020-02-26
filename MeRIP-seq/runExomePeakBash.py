b4e9d03993f2b1b153d1cfb50cfba0b64706424d
Keren Chow <34370908+KR-Chow@users.noreply.github.com>
2020-02-26 15:38:46 -0800

    Add files via upload

--- MeRIP-seq/runExomePeakBash.py
+++ MeRIP-seq/runExomePeakBash.py
@@ -12,44 +12,76 @@ from PubAlbum import Anno
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-gtf', action='store', type=str,
-                    default='hg38v31', help='gtf annotation build or file (eg. hg38v31)')
+                    default='hg38v33', help='gtf annotation build or file (eg. hg38v33)')
+parser.add_argument('-grepKept', action='store', type=str,
+                    help='regex for keeping files')
+parser.add_argument('-grepExpel', action='store', type=str,
+                    help='regex for filtering files')
 parser.add_argument('-input', action='store', type=str,
                     default='./', help='input bam directory (HepG2_shWTAP_IP_rep1.sorted.bam)')
-parser.add_argument('-index', action='store', type=str,
-                    default='hg38', help='bowtie2 genome index prefix or file (eg. hg38)')
+parser.add_argument('-log', action='store', type=str,
+                    default='./',
+                    help='log directory')
+parser.add_argument('-memory', action='store', type=str,
+                    default='50G', help='memory used for sbatch')
+parser.add_argument('-output', action='store', type=str,
+                    default='./',
+                    help='output folder')
+parser.add_argument('-script', action='store', type=str,
+                    default='./',
+                    help='directory for storing scripts')
 
 args = parser.parse_args()
 if len(sys.argv[1:]) == 0:
-    print("Running runExomePeakBash.py with defaultdict parameters...")
+    parser.print_help()
+    parser.exit()
 
 ##public arguments
-anno = Anno()
-gtf = anno.gtf(args.gtf)
-genomeIndex = anno.gindex(args.index)
+memory = args.memory
+gtf = args.gtf
+bashDir = args.script
+logDir = args.log
+output = args.output
 basepath = os.path.realpath(args.input)
 
-mainAlignDir = os.path.join(basepath, "alignment")
-exomePeakDir = os.path.join(basepath, "exomePeak")
-#filename: HepG2_control_input_rep1_run.sorted.bam, HepG2_control_input_rep2_run.sorted.bam
-bamList = sorted(glob(os.path.join(mainAlignDir, "*.bam")))
-bashDir = os.path.join(basepath, 'runBash')
-logDir = os.path.join(bashDir, 'log')
+if bool(args.grepKept):
+    kept = True
+    regex = re.compile(r'{0}'.format(args.grepKept))
+elif bool(args.grepExpel):
+    kept = False
+    regex = re.compile(r'{0}'.format(args.grepExpel))
+else:
+    regex = False
+
+#filename: HepG2_control_input_rep1.sorted.bam, HepG2_control_input_rep2.sorted.bam
+bamFiles = sorted(glob(os.path.join(basepath, '**', '*.bam'), recursive=True))
+bamList = list()
+for bamFile in bamFiles:
+    fileName = os.path.split(bamFile)[-1]
+    if bool(regex):
+        if kept:
+            if bool(regex.search(fileName)) is False:
+                continue
+        else:
+            if bool(regex.search(fileName)) is True:
+                continue
+    bamList.append(bamFile)
 
 bamDict = defaultdict(dict)
 for bam in bamList:
     basename = os.path.basename(bam)
-    cleanBasename = re.sub(r'\..+$', '', basename)
-    #HepG2 control IP rep1 run
+    cleanBasename = basename.split('.')[0]
+    #HepG2 control IP rep1
     basenameList = cleanBasename.split('_')
     ip = basenameList[2]
-    rep = "_".join(basenameList[3:5])
+    rep = basenameList[3]
     #HepG2_control
     baseExpName = '_'.join(basenameList[0:2])
     if baseExpName not in bamDict:
         bamDict[baseExpName] = defaultdict(dict)
-        bamDict[baseExpName][ip][rep] = basename
+        bamDict[baseExpName][ip][rep] = bam
     else:
-        bamDict[baseExpName][ip][rep] = basename
+        bamDict[baseExpName][ip][rep] = bam
 
 eachRTemplate = '''#!/usr/bin/env Rscript
 #exomepeak Script
@@ -57,15 +89,25 @@ eachRTemplate = '''#!/usr/bin/env Rscript
 #Define parameters and load library
 library("exomePeak")
 
-base = "{basepath}"
-setwd(paste(base, "alignment", sep = "/"))
+setwd("{output}")
 
 GTF_ANNO = "{gtf}"
 # peak calling
 exomepeak(GENE_ANNO_GTF=GTF_ANNO, 
   IP_BAM=c({ipBam}),
   INPUT_BAM=c({inputBam}), 
-  EXPERIMENT_NAME=paste("../exomePeak", "{rScriptBasename}", sep = "/"))
+  EXPERIMENT_NAME="./{rScriptBasename}")
+
+file.rename("./{rScriptBasename}/con_sig_diff_peak.bed", "./{rScriptBasename}.con_sig_diff_peak.bed")
+file.rename("./{rScriptBasename}/con_sig_diff_peak.xls", "./{rScriptBasename}.con_sig_diff_peak.xls")
+file.rename("./{rScriptBasename}/diff_peak.bed", "./{rScriptBasename}.diff_peak.bed")
+file.rename("./{rScriptBasename}/diff_peak.xls", "./{rScriptBasename}.diff_peak.xls")
+file.rename("./{rScriptBasename}/sig_diff_peak.bed", "./{rScriptBasename}.sig_diff_peak.bed")
+file.rename("./{rScriptBasename}/sig_diff_peak.xls", "./{rScriptBasename}.sig_diff_peak.xls")
+file.rename("./{rScriptBasename}/exomePeak.Rdata", "./{rScriptBasename}.exomePeak.Rdata")
+file.remove("./{rScriptBasename}")
+
+sessionInfo()
 '''
 
 sbatchTemplate = '''#!/bin/sh
@@ -75,26 +117,26 @@ sbatchTemplate = '''#!/bin/sh
 #SBATCH -n 1                          # Number of cores
 #SBATCH -N 1-1                        # Min - Max Nodes
 #SBATCH -p all                        # default queue is all if you don't specify
-#SBATCH --mem=100G                      # Amount of memory in GB
-#SBATCH --time=48:00:00               # Time limit hrs:min:sec
-#SBATCH --output={bashDir}/log/{rScriptBasename}.log   # Standard output and error log
+#SBATCH --mem={memory}                      # Amount of memory in GB
+#SBATCH --time=120:00:00               # Time limit hrs:min:sec
+#SBATCH --output={logDir}/{rScriptBasename}.log   # Standard output and error log
 
-base={bashDir}
+BASE={bashDir}
 
-Rscript $base/{rScriptBasename}.r
+Rscript $BASE/{rScriptBasename}.r
 '''
 
-runBashScript = os.path.join(bashDir, "runExomePeak.sh")
+runBashScript = os.path.join(bashDir, "runExomePeak.sbatch")
 with open(runBashScript, 'w') as runBashScriptO:
     runBashScriptO.write('#!/bin/sh\n')
-    runBashScriptO.write('base={0}\n\n'.format(bashDir))
+    runBashScriptO.write('BASE={0}\n\n'.format(bashDir))
     for baseExpName in sorted(bamDict.keys()):
         IPRepList = sorted(bamDict[baseExpName]['IP'].keys())
         inputRepList = sorted(bamDict[baseExpName]['input'].keys())
         if len(IPRepList) == len(inputRepList):
             repDict = defaultdict(dict)
             for rep in IPRepList:
-                repNum = re.findall(r'rep(\w+)_', rep)[0]
+                repNum = re.findall(r'rep(\w+)', rep)[0]
                 repDict[repNum]['IP'] = bamDict[baseExpName]['IP'][rep]
                 repDict[repNum]['input'] = bamDict[baseExpName]['input'][rep]
             repNumList = sorted(repDict.keys())
@@ -106,7 +148,7 @@ with open(runBashScript, 'w') as runBashScriptO:
                 combin = list(combin)
                 combinName = '-'.join(combin)
                 #HepG2_control_exomePeak_rep_1
-                rScriptBasename = '_'.join([baseExpName, 'exomePeak_rep_' + combinName])
+                rScriptBasename = '_'.join([baseExpName, 'rep' + combinName])
                 rScript = os.path.join(bashDir, rScriptBasename+'.r')
                 sbatchScript = os.path.join(bashDir, rScriptBasename + '.sbatch')
                 with open (rScript, 'w') as rScriptO, open (sbatchScript, 'w') as sbatchScriptO:
@@ -116,10 +158,10 @@ with open(runBashScript, 'w') as runBashScriptO:
                     rScriptO.write(rScriptCont)
                     sbatchScriptCont = sbatchTemplate.format(**vars())
                     sbatchScriptO.write(sbatchScriptCont)
-                runBashScriptO.write("sbatch $base/{rScriptBasename}.sbatch\n".format(**vars()))
+                runBashScriptO.write("sbatch $BASE/{rScriptBasename}.sbatch\n".format(**vars()))
         else:
             rScriptBasename = '_'.join([baseExpName, 'exomePeak_reps'])
-            rScript = os.path.join([bashDir, rScriptBasename+'.r'])
+            rScript = os.path.join(bashDir, rScriptBasename+'.r')
             sbatchScript = os.path.join(bashDir, '_'.join([baseExpName, 'exomePeak_reps.sbatch']))
             with open (rScript, 'w') as rScriptO, open (sbatchScript, 'w') as sbatchScriptO:
                 ipBam = ','.join(list(map(lambda x:'"{0}"'.format(x), bamDict[baseExpName]['IP'])))
@@ -128,5 +170,5 @@ with open(runBashScript, 'w') as runBashScriptO:
                 rScriptO.write(rScriptCont)
                 sbatchScriptCont = sbatchTemplate.format(**vars())
                 sbatchScriptO.write(sbatchScriptCont)
-            runBashScriptO.write("sbatch $base/{rScriptBasename}.sbatch\n".format(**vars()))
+            runBashScriptO.write("sbatch $BASE/{rScriptBasename}.sbatch\n".format(**vars()))
 os.chmod(runBashScript, 0o755)
