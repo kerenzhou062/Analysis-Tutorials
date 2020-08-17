f8ce4ffb9c9c70bebec09c46c43dc22619ee5c05
Keren Chow <34370908+KR-Chow@users.noreply.github.com>
2020-08-16 22:43:18 -0700

    Update coExpNetwork.py

--- RNA-seq/coExpNetwork.py
+++ RNA-seq/coExpNetwork.py
@@ -2,11 +2,12 @@
 import os
 import sys
 import re
+import argparse
 import numpy as np
 from scipy import stats
 import pandas as pd
-import argparse
 from multiprocessing import Pool
+from collections import defaultdict
 import time
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -24,6 +25,10 @@ parser.add_argument('--index', action='store', type=int, required=True,
                     help='used #th column (0-based) of --input for DataFrame index (geneName, geneId, etc.)')
 parser.add_argument('--input', action='store', type=str, required=True,
                     help='input gene expression matrix (column:sample, row:gene, "na" values will be filtered before calculation)')
+parser.add_argument('--method', action='store', type=str,
+                    choices=['spearman', 'pearson'],
+                    default='pearson',
+                    help='method to calculate the coefficient')
 parser.add_argument('--minsize', action='store', type=int,
                     default=10,
                     help='minimun sample size for calculating pearson correlation coefficient')
@@ -64,7 +69,7 @@ if len(sys.argv[1:]) == 0:
 
 def FiltDataframe(df, operator, operval, opertype):
     ## filter columns by row values
-    if (type(operator) is float) and (type(operval) is float):
+    if (type(operator) is str) and (type(operval) is float):
         if opertype == 'single':
             if operator == '<':
                 df =df[df.iloc[:,[0]] < operval]
@@ -92,10 +97,10 @@ def FiltDataframe(df, operator, operval, opertype):
     df = df.dropna(axis=0, how='any')
     return df
 
-def CallCoExpNetwork(data, igene, tgeneList, minSize, operators, opervals, opertype):
+def CallCoExpNetwork(data, qgene, tgeneList, minSize, operators, opervals, opertype, method):
     # get expression data of input gene
-    igAllData = data[[igene]]
-    coefList = list()
+    igAllData = data[[qgene]]
+    coefDict = defaultdict(list)
     # igAllData may contain multiple rows
     for i in range(len(igAllData.columns)):
         igData = igAllData.iloc[:, [i]]
@@ -104,51 +109,56 @@ def CallCoExpNetwork(data, igene, tgeneList, minSize, operators, opervals, opert
             for i in range(len(operators)):
                 igData = FiltDataframe(igData, operators[i], opervals[i], opertype)
         for tgene in tgeneList:
-            if tgene != igene:
-                # get expression data of testing gene
-                tgAllData = data[[tgene]]
-                rho = 0
-                pval = 1
-                sampleSize = 0
-                for j in range(len(tgAllData.columns)):
-                    # tgAllData may contain multiple rows
-                    tgData = tgAllData.iloc[:, [j]]
-                    # filter out values
-                    if opertype == 'single':
-                        for k in range(len(operators)):
-                            tgData = FiltDataframe(tgData, operators[k], opervals[k], opertype)
-                            ## get data from common columns and flatten
-                        mergeData = pd.concat([igData, tgData], axis=1)
-                        mergeData = FiltDataframe(mergeData, False, False, False)
-                    else:
-                        # Concatenate igData and tgData
-                        mergeData = pd.concat([igData, tgData], axis=1)
-                        mergeData = FiltDataframe(mergeData, False, False, False)
-                        for k in range(len(operators)):
-                            # filter out values on multiple rows
-                            mergeData = FiltDataframe(mergeData, operators[k], opervals[k], opertype)
-                    nparrA = mergeData[igene].astype('float64').to_numpy()
-                    nparrB = mergeData[tgene].astype('float64').to_numpy()
-                    ## to avoid 0 elements
-                    if len(nparrA) == 0:
-                        continue
-                    ## to avoid PearsonRConstantInputWarning: constant values
-                    if np.all(nparrA == nparrA[0]) or np.all(nparrB == nparrB[0]):
-                        continue
-                    tsampleSize = len(nparrA)
-                    if tsampleSize >= minSize:
-                        if args.log2 is True:
-                            nparrA = np.log2(nparrA + 0.01)
-                            nparrB = np.log2(nparrB + 0.01)
+            rho = 0
+            pval = 1
+            sampleSize = 0
+            # get expression data of testing gene
+            tgAllData = data[[tgene]]
+            for j in range(len(tgAllData.columns)):
+                # tgAllData may contain multiple rows
+                tgData = tgAllData.iloc[:, [j]]
+                # filter out values
+                if opertype == 'single':
+                    for k in range(len(operators)):
+                        tgData = FiltDataframe(tgData, operators[k], opervals[k], opertype)
+                        ## get data from common columns and flatten
+                    mergeData = pd.concat([igData, tgData], axis=1)
+                    mergeData = FiltDataframe(mergeData, False, False, False)
+                else:
+                    # Concatenate igData and tgData
+                    mergeData = pd.concat([igData, tgData], axis=1)
+                    mergeData = FiltDataframe(mergeData, False, False, False)
+                    for k in range(len(operators)):
+                        # filter out values on multiple rows
+                        mergeData = FiltDataframe(mergeData, operators[k], opervals[k], opertype)
+                nparrA = mergeData[qgene].astype('float64').to_numpy()
+                nparrB = mergeData[tgene].astype('float64').to_numpy()
+                ## to avoid 0 elements
+                if len(nparrA) == 0:
+                    continue
+                ## to avoid PearsonRConstantInputWarning: constant values
+                if np.all(nparrA == nparrA[0]) or np.all(nparrB == nparrB[0]):
+                    continue
+                tsampleSize = len(nparrA)
+                if tsampleSize >= minSize:
+                    if args.log2 is True:
+                        nparrA = np.log2(nparrA + 0.01)
+                        nparrB = np.log2(nparrB + 0.01)
+                    if method == 'pearson':
                         trho, tpvalue = stats.pearsonr(nparrA, nparrB)
-                        if abs(rho) < abs(trho):
-                            rho = trho
-                            pvalue = tpvalue
-                            sampleSize = tsampleSize
+                    elif method == 'spearman':
+                        trho, tpvalue = stats.spearmanr(nparrA, nparrB)
+                    if abs(rho) < abs(trho):
+                        rho = trho
+                        pvalue = tpvalue
+                        sampleSize = tsampleSize
                 if rho != 0:
-                    coefRow = [igene, tgene, str(rho), str(pvalue), str(sampleSize)]
-                    coefList.append(coefRow)
-    return coefList
+                    if tgene not in coefDict:
+                        coefDict[tgene] = [rho, pvalue, sampleSize]
+                    else:
+                        if abs(rho) > coefDict[tgene][2]:
+                            coefDict[tgene] = [rho, pvalue, sampleSize]
+    return [qgene, coefDict]
 
 # record start time
 stime = time.time()
@@ -204,18 +214,18 @@ resultList = []
 
 if 'all' in args.gene:
     for i in range(len(geneList) - 1):
-        igene = geneList[i]
+        qgene = geneList[i]
         tgeneList = geneList[i+1:]
-        result = pool.apply_async(CallCoExpNetwork, args=(data, igene, tgeneList, args.minsize, args.operator, args.operval, args.opertype,))
+        result = pool.apply_async(CallCoExpNetwork, args=(data, qgene, tgeneList, args.minsize, args.operator, args.operval, args.opertype,))
         resultList.append(result)
 else:
-    for igene in args.gene:
-        if igene not in geneList:
-            sys.stderr.write('No such gene ({0}) found in the expression matrix!\n'.format(igene))
+    for qgene in args.gene:
+        if qgene not in geneList:
+            sys.stderr.write('No such gene ({0}) found in the expression matrix!\n'.format(qgene))
             sys.exit()
-    for igene in args.gene:
+    for qgene in args.gene:
         tgeneList = list(filter(lambda x:x != args.gene, geneList))
-        result = pool.apply_async(CallCoExpNetwork, args=(data, igene, tgeneList, args.minsize, args.operator, args.operval, args.opertype,))
+        result = pool.apply_async(CallCoExpNetwork, args=(data, qgene, tgeneList, args.minsize, args.operator, args.operval, args.opertype,))
         resultList.append(result)
 pool.close()
 pool.join()
@@ -224,9 +234,10 @@ with open(args.output, 'w') as out:
     row = ['inputGene', 'testGene', 'pcc', 'pvalue', 'sampleSize']
     out.write('\t'.join(row) + '\n')
     for result in resultList:
-        coefList = result.get()
-        for coefRow in coefList:
-            out.write('\t'.join(coefRow) + '\n')
+        qgene, coefDict = result.get()
+        for tgene in sorted(coefDict.keys()):
+            coefRow = [qgene, tgene] + coefDict[tgene]
+            out.write('\t'.join(map(str, coefRow)) + '\n')
 
 if args.time:
     print("Running time is: %s seconds." % (time.time() - stime))
